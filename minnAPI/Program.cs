46ff7942c88197520bcdd9030c540b178b47bc66
seydem14 <seymanurdemirbas14@gmail.com>
2022-08-28 02:55:15 +0300

    .net 6 api üzerinden get isteği atma

---
+++ minnAPI/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+});
+
+
+app.MapGet("/200", () => "Merhabaa");
+app.Run();
